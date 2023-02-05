54a60f972b5399e526fbb941c09fbb0f80bdf193
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-05 06:59:13 +0100

    Creating a minimal PDF for each excel file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 # The glob module finds all the pathnames
 filepaths = glob.glob("invoices/*xlsx")
@@ -7,4 +9,13 @@ filepaths = glob.glob("invoices/*xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1") # Sheet_name je ime lista v Excelu
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    filename = Path(filepath).stem # to nam vrne samo ime datoteke npr "10001-2023.1.18.xlsx"
+    invoice_nr = filename.split("-")[0]
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+
+    pdf.output(f"PDFs/{filename}.pdf")
